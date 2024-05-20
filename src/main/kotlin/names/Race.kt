535a154e4e35952c4f385bcc54b16713c3b709cd
Josh Waterhouse <josh@cordiance.com>
2024-05-20 13:59:05 -0700

    feat: create Pict name generation

--- src/main/kotlin/names/Race.kt
+++ src/main/kotlin/names/Race.kt
@@ -2,79 +2,77 @@ package names
 
 enum class Race(val raceName: String) : NameGenerator {
     COMMON("Common") {
-        override fun generateName(gender: Boolean?): String = CommonNames.generateFullName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = CommonNames.generateFullName(isFemale ?: false)
 
     },
     AMAZON("Amazon") {
-        override fun generateName(gender: Boolean?): String = AmazonNames.generateFullName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = AmazonNames.generateFullName(isFemale ?: false)
     },
     ANGLO_SAXON("Anglo-Saxon") {
-        override fun generateName(gender: Boolean?): String = AngloSaxonNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = AngloSaxonNames.generateName(isFemale ?: false)
     },
     ATLATEAN("Atlatean") {
-        override fun generateName(gender: Boolean?): String = AtlanteanNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = AtlanteanNames.generateName(isFemale ?: false)
     },
     CARO_FRANK("Carolingian Frankish") {
-        override fun generateName(gender: Boolean?): String = CarolingianFrankishNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = CarolingianFrankishNames.generateName(isFemale ?: false)
     },
     ESQUIMAUX("Esquimaux") {
-        override fun generateName(gender: Boolean?): String = EsquimauxNames.generateName()
+        override fun generateName(isFemale: Boolean?): String = EsquimauxNames.generateName()
     },
     ESQ_IXIAN("Esquimaux-Ixian") {
-        override fun generateName(gender: Boolean?): String = EsquimauxIxianNames.generateName()
+        override fun generateName(isFemale: Boolean?): String = EsquimauxIxianNames.generateName()
     },
     GREEK("Greek") {
-        override fun generateName(gender: Boolean?): String = GreekNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = GreekNames.generateName(isFemale ?: false)
     },
     HYPERBOREAN("Hyperborean") {
-        override fun generateName(gender: Boolean?): String = HyperboreanNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = HyperboreanNames.generateName(isFemale ?: false)
     },
     IXIAN("Ixian") {
-        override fun generateName(gender: Boolean?): String = IxianNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = IxianNames.generateName(isFemale ?: false)
     },
     KELTIC("Keltic") {
-        override fun generateName(gender: Boolean?): String = KelticNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = KelticNames.generateName(isFemale ?: false)
     },
     KIMMERIAN("Kimmerian") {
-        override fun generateName(gender: Boolean?): String = KimmerianNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = KimmerianNames.generateName(isFemale ?: false)
     },
     KRIMMEAN("Krimmean") {
-        override fun generateName(gender: Boolean?): String = KimmerianNames.generateKrimmeanName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = KimmerianNames.generateKrimmeanName(isFemale ?: false)
     },
     LAPP("Lapp") {
-        override fun generateName(gender: Boolean?): String = LappNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = LappNames.generateName(isFemale ?: false)
     },
     LEMURIAN("Lemurian") {
-        override fun generateName(gender: Boolean?): String = LemurianNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = LemurianNames.generateName(isFemale ?: false)
     },
     MOORISH("Moorish") {
-        override fun generateName(gender: Boolean?): String = MoorishNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = MoorishNames.generateName(isFemale ?: false)
     },
     MU("Mu") {
-        override fun generateName(gender: Boolean?): String = MuNames.generateName(gender ?: false)
+        override fun generateName(isFemale: Boolean?): String = MuNames.generateName(isFemale ?: false)
     },
     PICTISH("Pictish") {
-        override fun generateName(gender: Boolean?): String {
-            TODO("Not yet implemented")
-        }
+        override fun generateName(isFemale: Boolean?): String = PictishNames.generateName(isFemale ?: false)
     },
     ROMAN("Roman") {
-        override fun generateName(gender: Boolean?): String {
+        override fun generateName(isFemale: Boolean?): String {
             TODO("Not yet implemented")
         }
     },
     TLINGIT("Tlingit") {
-        override fun generateName(gender: Boolean?): String {
+        override fun generateName(isFemale: Boolean?): String {
             TODO("Not yet implemented")
         }
     },
     VIKING("Viking") {
-        override fun generateName(gender: Boolean?): String {
+        override fun generateName(isFemale: Boolean?): String {
             TODO("Not yet implemented")
         }
     },
     Yakut("Yakut") {
-        override fun generateName(gender: Boolean?): String {
+        override fun generateName(isFemale: Boolean?): String {
             TODO("Not yet implemented")
         }
     }
